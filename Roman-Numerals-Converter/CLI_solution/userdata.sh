5151fdb001cc70f7092cbc2e355dc94fa18ffbaa
Alparslan <alparslanu6347@gmail.com>
2023-01-22 23:54:22 +0300

    Roman-Numerals-Converter user-data değiştirildi.

--- Roman-Numerals-Converter/CLI_solution/userdata.sh
+++ Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -4,7 +4,7 @@ yum install python3 -y
 pip3 install flask
 yum install git -y
 cd /home/ec2-user
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/index.html
-wget -P templates https://raw.githubusercontent.com/serdarcw/cli_deneme/master/templates/result.html
-wget https://raw.githubusercontent.com/serdarcw/cli_deneme/master/app.py
+wget -P templates https://raw.githubusercontent.com/alparslanu6347/my-projects/main/Roman-Numerals-Converter/templates/index.html
+wget -P templates https://raw.githubusercontent.com/alparslanu6347/my-projects/main/Roman-Numerals-Converter/templates/result.html
+wget https://raw.githubusercontent.com/alparslanu6347/my-projects/main/Roman-Numerals-Converter/app.py
 python3 app.py
