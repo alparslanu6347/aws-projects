97063356d975761f1d0c7064e821eed9951a62a8
Alparslan <alparslanu6347@gmail.com>
2023-01-23 20:57:39 +0300

    1.versiyon

--- Roman-Numerals-Converter-Application/userdata.sh
+++ Roman-Numerals-Converter-Application/userdata.sh
@@ -4,6 +4,6 @@ yum install python3 -y
 pip3 install flask
 cd /home/ec2-user
 wget -P templates https://raw.githubusercontent.com/XXXXXXXXX/my-projects/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/index.html
-wget -P templates https://raw.githubusercontent.com/XXXXXXXXX/my-projects/main/aws/projects/Project-001-Roman-Numerals-Converter/templates/result.html
-wget https://raw.githubusercontent.com/XXXXXXXXX/my-projects/main/aws/projects/Project-001-Roman-Numerals-Converter/app.py
+wget -P templates https://raw.githubusercontent.com/alparslanu6347/my-projects/main/Roman-Numerals-Converter-Application/templates/result.html
+wget https://raw.githubusercontent.com/alparslanu6347/my-projects/main/Roman-Numerals-Converter-Application/app.py
 python3 app.py
