f3c92cb4786963f7b7a2647c0d9e6e4b51d32505
alparslanu6347 <alparslanu6347@gmail.com>
2024-03-04 17:05:24 +0300

    unnecessary files deleted

--- linux_project_19-1-2023/Project-101/Forth_Part/Task_1/command.sh
+++
@@ -1,2 +0,0 @@
-1.Bolum : 
-sed -i "s/ec2-private_ip/$(grep PrivateIpAddress info.json | head -n1 | cut -d'"' -f4)/g" terraform.tf
--- linux_project_19-1-2023/Project-101/Forth_Part/Task_2/solution.sh
+++
@@ -1,5 +0,0 @@
-MY_KEY=$(cat certificate.pem)
-
-echo $MY_KEY # This command is used for seeing the difference between echo -e 
-
-echo -e $MY_KEY > new.pem
